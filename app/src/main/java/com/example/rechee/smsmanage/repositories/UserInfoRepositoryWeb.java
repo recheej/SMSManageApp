17684ea0ff8d9add219687135d053bb3829a6bdf
Rechee <recheejozil@hotmail.com>
2017-06-17 20:16:12 -0400

    Edit of suer info service

--- app/src/main/java/com/example/rechee/smsmanage/repositories/UserInfoRepositoryWeb.java
+++ app/src/main/java/com/example/rechee/smsmanage/repositories/UserInfoRepositoryWeb.java
@@ -1,11 +1,15 @@
 package com.example.rechee.smsmanage.repositories;
 
 import com.example.rechee.smsmanage.http.interfaces.toggl.UserInfoService;
+import com.example.rechee.smsmanage.models.TogglResponse;
 import com.example.rechee.smsmanage.models.UserInfo;
 import com.example.rechee.smsmanage.repositories.interfaces.UserInfoRepository;
 
 import javax.inject.Inject;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 
 /**
@@ -22,7 +26,17 @@ public class UserInfoRepositoryWeb extends WebRepository implements UserInfoRepo
     @Override
     public UserInfo getUserInfo(String username, String password) {
         UserInfoService userInfoService = retrofit.create(UserInfoService.class);
+        return userInfoService.userInformation().enqueue(new Callback<TogglResponse<UserInfo>>() {
+            @Override
+            public void onResponse(Call<TogglResponse<UserInfo>> call, Response<TogglResponse<UserInfo>> response) {
 
+            }
+
+            @Override
+            public void onFailure(Call<TogglResponse<UserInfo>> call, Throwable t) {
+
+            }
+        });
         return new UserInfo();
     }
 }
